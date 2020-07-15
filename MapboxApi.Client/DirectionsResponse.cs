1299d269e2efeaf663c3dd8b160f0fbd7444907b
Robin Carter <Robin.Carter@sse.com>
2020-07-15 15:16:12 +0100

    Update the direction response

--- MapboxApi.Client/DirectionsResponse.cs
+++ MapboxApi.Client/DirectionsResponse.cs
@@ -1,8 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MapboxApi.Client
 {
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    public class Leg
+    {
+        public string Summary { get; set; }
+        public List<object> Steps { get; set; }
+        public double Distance { get; set; }
+        public double Duration { get; set; }
+        public double Weight { get; set; }
+    }
+
+    public class Geometry
+    {
+        public List<List<double>> Coordinates { get; set; }
+        public string Type { get; set; }
+    }
+
+    public class Route
+    {
+        public string Weight_name { get; set; }
+        public List<Leg> Legs { get; set; }
+        public Geometry Geometry { get; set; }
+        public double Distance { get; set; }
+        public double Duration { get; set; }
+        public double Weight { get; set; }
+    }
+
+    public class Waypoint
+    {
+        public double Distance { get; set; }
+        public string Name { get; set; }
+        public List<double> Location { get; set; }
+    }
+
     public class DirectionsResponse
     {
+        public List<Route> Routes { get; set; }
+        public List<Waypoint> Waypoints { get; set; }
+        public string Code { get; set; }
+        public string Uuid { get; set; }
     }
 }
--- MapboxApi/MapboxController.cs
+++ MapboxApi/MapboxController.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Net.Http;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using MapboxApi.Client;
 
 namespace MapboxApi
 {
@@ -30,8 +31,8 @@ namespace MapboxApi
 
             try
             {
-                //var response = await _client.GetAsync($"");
-                //response.EnsureSuccessStatusCode();
+                var response = await _client.GetAsync("https://api.mapbox.com/directions/v5/mapbox/");
+                response.EnsureSuccessStatusCode();
             }
             catch (Exception exception)
             {
@@ -39,6 +40,50 @@ namespace MapboxApi
             }
 
             return new OkObjectResult(HealthCheckResult.Healthy("A healthy result."));
+        }
+
+        [FunctionName("Route")]
+        public async Task<IActionResult> RunGetPlaces(
+           [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+           ILogger log)
+        {
+            DirectionsResponse result = new DirectionsResponse();
+
+            try
+            {
+                log.LogInformation("C# HTTP trigger function processed a request.");
+
+                var routeTypeQueryParameter = req.Query["routetype"].ToString();
+                if (string.IsNullOrEmpty(routeTypeQueryParameter))
+                    return new BadRequestObjectResult("Please pass a route type parameter");
+
+                var startLatQueryParameter = req.Query["startlat"].ToString();
+                if (string.IsNullOrEmpty(startLatQueryParameter))
+                    return new BadRequestObjectResult("Please pass a lat parameter");
+
+                var startLonQueryParameter = req.Query["startlon"].ToString();
+                if (string.IsNullOrEmpty(startLonQueryParameter))
+                    return new BadRequestObjectResult("Please pass a lon parameter");
+
+                var endLatQueryParameter = req.Query["endlat"].ToString().Trim('f', 'F');
+                if (string.IsNullOrEmpty(endLatQueryParameter))
+                    return new BadRequestObjectResult("Please pass a lat parameter");
+
+                var endLonQueryParameter = req.Query["endlon"].ToString().Trim('f', 'F');
+                if (string.IsNullOrEmpty(endLonQueryParameter))
+                    return new BadRequestObjectResult("Please pass a lon parameter");
+
+                // Need to move access key secure storage
+                _client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json; charset=utf-8");
+                var response = await _client.GetAsync($"https://api.mapbox.com/directions/v5/mapbox/{routeTypeQueryParameter}/{startLatQueryParameter}%2C{startLonQueryParameter}%3B{endLatQueryParameter}%2C{endLonQueryParameter}?alternatives=false&geometries=geojson&steps=false&access_token=pk.eyJ1Ijoicndvb2xsY290dCIsImEiOiJja2FnaWlsMHQwNnYyMnpvNWhhbTd1OTRiIn0.5pL3D0LvtE8A6Yuz40RhIA");
+                result = await response.Content.ReadAsAsync<DirectionsResponse>();
+            }
+            catch (Exception exception)
+            {
+                // Error
+            }
+
+            return new OkObjectResult(result);
         }
     }
 }
